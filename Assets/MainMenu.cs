7f92f62ac2ea8aeb99e6b963b05e2760e80d4a6c
hxtruong <hoangxuantruong98@gmail.com>
2020-01-18 03:53:52 +0700

    feat: menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string PlayScence = "TestMap";
+    public GameObject SettingPanel;
+    public GameObject GuidelinePanel;
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadYourAsyncScene(PlayScence));
+    }
+
+    public void SettingGameToggle()
+    {
+        SettingPanel.SetActive(!SettingPanel.activeSelf);
+    }
+
+    public void GuidelineGameToggle()
+    {
+        GuidelinePanel.SetActive(!GuidelinePanel.activeSelf);
+    }
+
+    IEnumerator LoadYourAsyncScene(string scence)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scence);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+}
